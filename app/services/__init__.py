551657cff9c0980ed332c9f21a07e9943481d760
Maksim <maksimgubenin@mail.ru>
2026-01-18 09:38:42 +0000

    feat: Create base Deribit client

---
+++ app/services/__init__.py
@@ -0,0 +1,5 @@
+from app.services.deribit_client import DeribitClient
+
+__all__ = (
+    "DeribitClient",
+)
---
+++ app/services/deribit_client.py
@@ -0,0 +1,15 @@
+import aiohttp
+from decimal import Decimal
+
+class DeribitClient:
+    def __init__(self):
+        self.base_url = "https://www.deribit.com/api/v2/public/{action}"
+
+    async def get_index_price(self, ticker: str) -> dict:
+        action = "get_index_price?index_name={ticker}".format(ticker=ticker)
+        url = self.base_url.format(action=action)
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as response:
+                response.raise_for_status()
+                data = await response.json()
+                return data
